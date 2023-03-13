1bbb05604b4e2e50c5f3c20ff760614d4de3cbac
Kuba <brozdajakub@gmail.com>
2023-03-13 19:40:06 +0100

    Fix CORS

--- src/main/java/pl/reservo/reservoservice/endpoint/CompanyController.java
+++ src/main/java/pl/reservo/reservoservice/endpoint/CompanyController.java
@@ -2,11 +2,7 @@ package pl.reservo.reservoservice.endpoint;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.reservo.reservoservice.api.CompanyRegistrationRequest;
 import pl.reservo.reservoservice.api.CompanyRegistrationResponse;
 import pl.reservo.reservoservice.service.CompanyService;
@@ -16,6 +12,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/companies")
 @RequiredArgsConstructor
+@CrossOrigin
 class CompanyController {
 
   private final CompanyService companyService;
